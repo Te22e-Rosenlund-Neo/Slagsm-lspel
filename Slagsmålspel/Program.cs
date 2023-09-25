353a93ec7b873c3a5f46febd353bd8e94eb69a7b
Neo Rosenlund <Neo.rosenlund@elev.ga.ntig.se>
2023-09-25 21:33:25 +0200

    added character and Foreground color change method

--- Slagsmålspel/Program.cs
+++ Slagsmålspel/Program.cs
@@ -2,6 +2,7 @@
 using System.Security.Cryptography;
 
 //lägg till så att man kan skapa nya karaktärer
+//ascci art
 
 
 
@@ -12,12 +13,13 @@ Random Generator1 = new Random();
 //Karaktärernas stats återspeglar inte åsikter, karaktärsdrag eller liknande om personen/na!
 // character x = new character(name,damage,health,armor,speed,move1,move1damage,move1hitchance,move2,move2damage,move2hitchance,id)
 Character Player1 = new Character("Micke", 60,100,40,50,"Skjut lasrar ur dina röda ögon",1,0.9,"Ta hjälp av japanare",3,0.4,1);
-Character Player2 = new Character("Martin",80, 110, 30,30,"använd TOUCHPADEN!",2,0.7,"använd din mus",0.9,1,2);
-Character Player3 = new Character("Huglin",70, 60, 20,100,"Goblin GANG", 1.5,0.8,"Dissa skolmaten",1,1,3);
-Character Player4 = new Character("Vera",30, 50, 100,70,"DU har redovisnig imorgon", 2,0.7,"Fäll ner din skärm",1,0.9,4);
-Character Player5 = new Character("Nicholas",20, 120, 100,10,"ANVÄND NEWTONS 0'TE LAG",10,0.2,"Mät motståndaren med plantan!",1.2,0.9,5);
-Character Player6 = new Character("Rådet",100, 50, 10,90,"Bryt era egna regler, genom korrupta handlingar",1,1,"laga en 3d-printer",2,0.7,6);
-Character[] Players = {Player1,Player2,Player3,Player4,Player5,Player6};
+Character Player2 = new Character("Martin",80, 110, 30,30,"använd TOUCHPADEN!",2,0.7,"använd din mus, primitivt!",0.9,0.9,2);
+Character Player3 = new Character("Huglin",70, 60, 20,100,"Goblin GANG", 1.5,0.8,"Dissa skolmaten",1,0.95,3);
+Character Player4 = new Character("Vera",30, 50, 100,70,"DU har redovisnig imorgon", 2,0.7,"Se maxis långfinger och använd 100000J i attack",4,0.5,4);
+Character Player5 = new Character("Nicholas",20, 120, 100,10,"ANVÄND NEWTONS 0'TE LAG",10,0.3,"Mät motståndaren med KrukVäxten i sal A6!",1.2,0.9,5);
+Character Player6 = new Character("Rådet",100, 50, 10,90,"Bryt era egna regler, genom korrupta handlingar",1,0.9,"laga en 3d-printer",2,0.7,6);
+Character Player7 = new Character("Atom-bomb",201,49,0,0,"tsar-bomba",10,0.3,"nagasaki",0.1,1,7);
+Character[] Players = {Player1,Player2,Player3,Player4,Player5,Player6,Player7};
 Character p1;
 Character p2;
 //----------------------------------------------------------------------------------------
@@ -83,7 +85,19 @@ if(phase == "characterdecision"){
 }
 }
 }
+//----------------------------------------------------------------------------------------
+void colorswap(string Text, string color){
+if(color == "Red"){
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine(Text);
+    Console.ResetColor();
+}else if(color == "Green"){
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine($"{Text}");
+    Console.ResetColor();
+}
 
+}
 //----------------------------------------------------------------------------------------
 void GameOpeningChoice(){
     //launches at game start
@@ -135,10 +149,7 @@ if(Convert.ToInt32(Answer) == 1){
 else if(Convert.ToInt32(Answer) == 2){
         p2.Hp -= p1.Attack2DamageCalculator(p2.Armour);
     }
-    Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Player 2 has {p2.Hp}hitpoints left");
-    Console.ResetColor();
-
+    colorswap($"Player 2 has {p2.Hp} hitpoints left","Red");
 //----
 //same as above, but if the attacker is player2
 }else if(attacker == "player2"){
@@ -152,10 +163,8 @@ if(Convert.ToInt32(Answer) == 1){
 else if(Convert.ToInt32(Answer) == 2){
         p1.Hp -= p2.Attack2DamageCalculator(p1.Armour);
     }
-        Console.ForegroundColor = ConsoleColor.Red;
-    Console.WriteLine($"Player 1 has {p1.Hp}hitpoints left");
-        Console.ResetColor();
-        
+    colorswap($"Player 1 has {p1.Hp} hitpoints left","Red");
+
 }
 }
 //----------------------------------------------------------------------------------------
@@ -164,45 +173,43 @@ int round = 0;
 while(true){
     round++;
     Console.Clear();
-    Console.ForegroundColor = ConsoleColor.Green;
-    Console.WriteLine($"Round: {round}");
-    Console.ResetColor();
+    colorswap($"Round: {round}","Green");
     Console.WriteLine($"player1's {p1.Name} has {p1.Hp} health.         Player2's {p2.Name} has {p2.Hp} health.");
 //the different types of possibilities depending on who is quicker
 if(p1.Speed > p2.Speed){
     attackphase("player1");
     attackphase("player2");
-    Thread.Sleep(4000);
+    Thread.Sleep(3000);
 }else if(p1.Speed < p2.Speed){
     attackphase("player2");
     attackphase("player1");
-    Thread.Sleep(4000);
+    Thread.Sleep(3000);
 }else if(p1.Speed == p2.Speed){
     int randomorder = Generator1.Next(1,2);
         if(randomorder == 1){
             attackphase("player1");
             attackphase("player2");
-            Thread.Sleep(4000);
+            Thread.Sleep(3000);
     }else if(randomorder == 2){
             attackphase("player2");
             attackphase("player1");
-            Thread.Sleep(4000);
+            Thread.Sleep(3000);
     }
 }
 //checks which player won
 if(p1.Hp <= 0){
-    Console.WriteLine($"Player2's {p2.Name} won!!");
+    colorswap($"Player2's {p2.Name} won!!", "Green");
     break;
 }else if(p2.Hp <= 0){
-    Console.WriteLine($"Player1's {p1.Name} won!!");
+    colorswap($"Player1's {p1.Name} won!!", "Green");
     break;
 }else if(p1.Hp <=0 && p2.Hp <= 0){
-    Console.WriteLine($"The game ended in a stalemate!!");
+    colorswap($"The game ended in a stalemate!!", "Green");
     break;
 }
 
 }
-Console.WriteLine("press any button to continue");
+Console.WriteLine("\npress any button to continue");
 Console.ReadKey();
 gamemenu();
 }
